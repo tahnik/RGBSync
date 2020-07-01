1cde743ed6dfcea534173eabe364902387e6b546
Tahnik Mustasin <tahnikm@gmail.com>
2020-07-01 11:18:56 +0100

    Remove XML dependencies from Gigabyte

--- Gigabyte/Gigabyte.cs
+++ Gigabyte/Gigabyte.cs
@@ -14,8 +14,6 @@ namespace SDKs
         }
 
         private RGBFusion fusion_;
-        private const int TIMEPERFRAME = 500;
-        private int duration_ = 30000;
         private Mode currentMode_ = Mode.INITIAL;
 
         public Gigabyte()
@@ -26,11 +24,6 @@ namespace SDKs
         public void Init()
         {
             fusion_.Init();
-        }
-
-        public void SetDuration(int duration)
-        {
-            duration_ = duration;
         }
 
         public void ToGameMode()
@@ -46,13 +39,8 @@ namespace SDKs
             }
             else
             {
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
-
                 for (double i = 0; i <= 255; i += 1)
                 {
-                    // int val = (int)((1 - Math.Pow(i - 1, 2)) * 255);
-
                     byte r = (byte)i;
                     byte g = (byte)(255 - i);
                     byte b = (byte)(255 - i);
@@ -61,8 +49,6 @@ namespace SDKs
                     fusion_.SetAllAreas(Color.FromRgb(r, g, b));
                 }
 
-                sw.Stop();
-                Console.WriteLine("Elapsed={0}", sw.Elapsed);
 
             }
             currentMode_ = Mode.GAME;
@@ -80,11 +66,8 @@ namespace SDKs
             }
             else
             {
-                // for (double i = 0; i < 1; i += (double)TIMEPERFRAME / duration_)
                 for (double i = 0; i <= 255; i += 1)
                 {
-                    // int val = (int)((1 - Math.Pow(i - 1, 2)) * 255);
-
                     byte r = (byte)(255 - i);
                     byte g = (byte)i;
                     byte b = (byte)i;
--- Gigabyte/RGBFusion.cs
+++ Gigabyte/RGBFusion.cs
@@ -12,8 +12,6 @@ namespace SDKs
     {
 
         private Comm_LED_Fun ledFun_;
-        private List<CommUI.Area_class> allAreaInfo_;
-        private List<CommUI.Area_class> allExtAreaInfo_;
         private bool initialized_;
 
         public bool IsInitialized()
@@ -21,17 +19,7 @@ namespace SDKs
             return ledFun_ != null && initialized_;
         }
 
-        private void FillAllAreaInfo()
-        {
-            string areaFilename_ = "Pro1.xml";
-            allAreaInfo_ = CommUI.Inport_from_xml(areaFilename_, But_Style: null);
-        }
 
-        private void Fill_ExtArea_info()
-        {
-            string extAreaFileName_ = "ExtPro1.xml";
-            allExtAreaInfo_ = CommUI.Inport_from_xml(extAreaFileName_, But_Style: null);
-        }
         public void Init()
         {
             if (IsInitialized())
@@ -53,53 +41,16 @@ namespace SDKs
             }
             while (!scanDone);
 
-            ledFun_.Current_Mode = 0; // 1= Advanced 0 = Simple or Ez
+            ledFun_.Current_Mode = 0;
             ledFun_.Led_Ezsetup_Obj.PoweronStatus = 1;
             ledFun_.Set_Sync(false);
 
             initialized_ = true;
-
-            FillAllAreaInfo();
-            Fill_ExtArea_info();
         }
 
-        public void SetAllAreas(object obj)
+        public void SetAllAreas(Color color)
         {
-            Stopwatch sw = new Stopwatch();
-
-            if (allAreaInfo_ == null || allExtAreaInfo_ == null)
-            {
-                Console.WriteLine("Gigabyte areas are null!");
-                return;
-            }
-            Color color = (Color)obj;
-
-            var patternCombItem = new CommUI.Pattern_Comb_Item
-            {
-                Bg_Brush_Solid = { Color = (Color)obj },
-                Sel_Item = { Style = null }
-            };
-
-            patternCombItem.Sel_Item.Background = patternCombItem.Bg_Brush_Solid;
-            patternCombItem.Sel_Item.Content = string.Empty;
-            patternCombItem.But_Args = CommUI.Get_Color_Sceenes_class_From_Brush(patternCombItem.Bg_Brush_Solid);
-            patternCombItem.Bri = 9;
-            patternCombItem.Speed = 9;
-            patternCombItem.Type = 0;
-
-
-            var allAreaInfo = allAreaInfo_.Select(areaInfo => new CommUI.Area_class(patternCombItem, areaInfo.Area_index, mBut_Style: null)).ToList();
-
-            var allExtAreaInfo = allExtAreaInfo_.Select(areaInfo => new CommUI.Area_class(patternCombItem, areaInfo.Area_index, mBut_Style: null) { Ext_Area_id = areaInfo.Ext_Area_id }).ToList();
-
-            sw.Start();
-            // allAreaInfo.AddRange(allExtAreaInfo);
-            // ledFun_.Set_Adv_mode(allAreaInfo, Run_Direct: true);
             ledFun_.Set_CS_Effect(0, 9, 9, color.R, color.G, color.B);
-            // ledFun_.Set_Ezmode_Direct(LedLib2.LedMode.StillMode, (Color)obj);
-            sw.Stop();
-
-            Console.WriteLine("Elapsed SetAllAreas={0}", sw.Elapsed);
         }
 
     }
